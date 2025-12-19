9950d425fb740b8154f15189d05e0c7e4af3d2f9
wangyi01 <wangyi01@dp.tech>
2025-12-19 19:53:11 +0800

    build

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 set -e
-cd /root/build_agent || exit 1
-source /root/build_agent/.env || exit 1
+cd /root/build_tools || exit 1
+source /root/build_tools/.env || exit 1
 
-nohup /root/build_agent/.venv/bin/python run.py --all > log 2>&1 &
+nohup /root/build_tools/.venv/bin/python run.py --all > log 2>&1 &
 
 disown $!
 
